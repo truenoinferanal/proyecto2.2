b78b2dbbcc2bf04d09de7778e615cfd35b5b918f
truenoinferanal <108770749+truenoinferanal@users.noreply.github.com>
2022-07-27 10:12:13 -0500

    Add files via upload

---
+++ function a(){.js
@@ -0,0 +1,6 @@
+function a(){
+    var numero1= Number(document.getElementById("suma1").value);
+    var numero2= Number(document.getElementById("suma2").value);
+    var c= numero1+numero2;
+    document.getElementById("da").innerHTML = numero3;
+    }
